a58b388484e99351379d6bbe3157057df20beda4
chouske <132114322+chouske@users.noreply.github.com>
2023-11-24 23:15:00 -0800

    Added a way to plant(spacebar) and a way to harvest(almost done)

--- Game Dev Project/Assets/CropManager.cs
+++ Game Dev Project/Assets/CropManager.cs
@@ -1,16 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CropManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject[,] Board; //A 2d array where each row/column is the same size
+    CropCell[,] Board; //A 2d array where each row/column is the same size
     int boardSize = 10;
+    int counter = 0;
     void Start()
-    {
-        Board = new GameObject[boardSize, boardSize]; //Each "cell"'s area/square is from index x to index x+1, and index y to index y+1
-        RegenerateBoard();
+    { 
+        Board = new CropCell[boardSize, boardSize]; //Each "cell"'s area/square is from index x to index x+1, and index y to index y+1
+        for(int x = (0-(boardSize/2)); x < 0; x++){ //Left half of board ex: -5 to 0(not inclsuive)
+            for(int y = (0-(boardSize/2)); y < 0; y++){ 
+                Board[x + (boardSize/2), y + (boardSize/2)] = new CropCell(x, y);
+             }
+             for(int y = 1 ; y < ((boardSize/2) + 1); y++){ 
+                Board[x + (boardSize/2), y + (boardSize/2) - 1] = new CropCell(x, y);
+             }
+        }
+        for(int x = 1 ; x < ((boardSize/2) + 1); x++){ //Right half
+            for(int y = (0-(boardSize/2)); y < 0; y++){ 
+                Board[x + (boardSize/2) - 1, y + (boardSize/2)] = new CropCell(x, y);
+             }
+             for(int y = 1 ; y < ((boardSize/2) + 1); y++){ 
+                Board[x + (boardSize/2) - 1, y + (boardSize/2) - 1] = new CropCell(x, y);
+             }
+        }
+        //RegenerateBoard();
     }
 
     // Update is called once per frame
@@ -21,8 +41,98 @@ public class CropManager : MonoBehaviour
     void RegenerateBoard(){
         for(int i = 0; i < boardSize; i++){
             for(int j = 0; j < boardSize; j++){
-                Board[i, j] = new GameObject("crop: " + ((10*i) + (j+1)).ToString()); //To do: Only generate crops with a 30% chance
+                Board[i, j].Plant(); //To do: Only generate crops with a 30% chance
             }
         }
     }
+    public void PlayerPlant(float playerx, float playery){
+        CropCell theCell;
+        for(int x = 0; x < boardSize; x++){
+            for(int y = 0; y < boardSize; y++){
+                theCell = Board[x, y];
+                (float realx, float realy) = theCell.getRealCoordinates();
+                if((Math.Abs((realx - playerx)) < 1) && (Math.Abs((realy - playery)) < 1)){
+                    bool result = Board[x, y].Plant();
+                    if(result == true){
+                        x = boardSize;
+                        y = boardSize;
+                    }
+                }
+            }
+        }   
+    }
+    public void PlayerHarvest(float playerx, float playery){
+        CropCell theCell;
+        for(int x = 0; x < boardSize; x++){
+            for(int y = 0; y < boardSize; y++){
+                theCell = Board[x, y];
+                (float realx, float realy) = theCell.getRealCoordinates();
+                if((Math.Abs((realx - playerx)) < 1) && (Math.Abs((realy - playery)) < 1)){
+                    bool result = Board[x, y].Harvest();
+                    if(result == true){
+                        x = boardSize;
+                        y = boardSize;
+                    }
+                }
+            }
+        }   
+    }
+    public class CropCell{ //Planting a seed instantly makes the crop level 1
+        GameObject cropObject;
+        int sunLevel;
+        int waterLevel;
+        public float xpos;
+        public float ypos;
+        public CropCell(int xpos, int ypos){
+            cropObject = null; // Null means no seed or plant there
+            sunLevel = 0;
+            waterLevel = 0;
+            this.xpos = xpos;
+            this.ypos = ypos;
+        }
+        public void ResetCell(){
+            sunLevel = 0;
+            waterLevel = 0;
+            if(cropObject != null){
+                Destroy(cropObject);
+            }
+        }
+        public bool Plant(){ // Was planting successful?
+            if(cropObject == null){
+                cropObject = new GameObject("crop");
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        public bool Harvest(){
+            if(cropObject != null){
+                Destroy(cropObject);
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        public (float xpos, float ypos) getRealCoordinates(){
+            float realxpos = this.xpos;
+            float realypos = this.ypos;
+            if(realxpos > 0){
+                realxpos = (float)(realxpos - 0.5);
+            }
+            else{
+                realxpos = (float)(realxpos + 0.5);
+            }
+            if(realypos > 0){
+                realypos = (float)(realypos - 0.5);
+            }
+            else{
+                realypos = (float)(realypos + 0.5);
+            }
+            return (realxpos, realypos);
+        }
+
+    }
 }
+
--- Game Dev Project/Assets/PlayerController.cs
+++ Game Dev Project/Assets/PlayerController.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    enum moveState{    
-        still,
-        moving
+    enum jumpButtonState{    
+        up,
+        down
     }
-    moveState playerMoveState;
+    jumpButtonState jumpState;
     // Start is called before the first frame update
     void Start()
     {
-        playerMoveState = moveState.still;
+        jumpState = jumpButtonState.up;
     }
 
     // Update is called once per frame
@@ -30,8 +30,18 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.RightArrow)){
             transform.Translate(new Vector3(1.0f, 0.0f, 0.0f));
         }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            
+            if(jumpState == jumpButtonState.up){
+                gameObject.GetComponent<CropManager>().PlayerPlant(transform.position.x, transform.position.z);//Important that this is z, not y!
+                jumpState = jumpButtonState.down;
+            }
+        }
+        if(Input.GetKeyUp(KeyCode.Space)){
+            jumpState = jumpButtonState.up;
+        }
     }
-    void triggerTurn(){
+    void triggerTurn(){ //Once player does an action, this is triggered, should randomly add water or sun to cells or whatever
 
     }
 }
