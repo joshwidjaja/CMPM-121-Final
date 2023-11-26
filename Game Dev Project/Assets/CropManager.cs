7deed7cc02bcd962525ebaedd5b605100b9c98e4
Joshua Widjaja <josh@widjaja.us>
2023-11-25 18:28:26 -0800

    refactored CropManager

--- Game Dev Project/Assets/CropManager.cs
+++ Game Dev Project/Assets/CropManager.cs
@@ -9,25 +9,32 @@ public class CropManager : MonoBehaviour
 {
     // Start is called before the first frame update
     CropCell[,] Board; //A 2d array where each row/column is the same size
-    int boardSize = 10;
+    int BOARD_SIZE = 10;
     int counter = 0;
     void Start()
     { 
-        Board = new CropCell[boardSize, boardSize]; //Each "cell"'s area/square is from index x to index x+1, and index y to index y+1
-        for(int x = (0-(boardSize/2)); x < 0; x++){ //Left half of board ex: -5 to 0(not inclsuive)
-            for(int y = (0-(boardSize/2)); y < 0; y++){ 
-                Board[x + (boardSize/2), y + (boardSize/2)] = new CropCell(x, y);
+        Board = new CropCell[BOARD_SIZE, BOARD_SIZE]; //Each "cell"'s area/square is from index x to index x+1, and index y to index y+1
+        int UNIT_TILE = BOARD_SIZE / 2;
+        int FIRST_HALF_START = 0 - UNIT_TILE;
+        int FIRST_HALF_END = 0;
+
+        int SECOND_HALF_START = 1;
+        int SECOND_HALF_END = UNIT_TILE + 1;
+
+        for(int x = FIRST_HALF_START; x < FIRST_HALF_END; x++){ //Left half of board ex: -5 to 0(not inclusive)
+            for(int y = FIRST_HALF_START; y < FIRST_HALF_END; y++){ 
+                Board[x + (UNIT_TILE), y + (UNIT_TILE)] = new CropCell(x, y);
              }
-             for(int y = 1 ; y < ((boardSize/2) + 1); y++){ 
-                Board[x + (boardSize/2), y + (boardSize/2) - 1] = new CropCell(x, y);
+             for(int y = SECOND_HALF_START ; y < SECOND_HALF_END; y++){ 
+                Board[x + (UNIT_TILE), y + (UNIT_TILE) - 1] = new CropCell(x, y);
              }
         }
-        for(int x = 1 ; x < ((boardSize/2) + 1); x++){ //Right half
-            for(int y = (0-(boardSize/2)); y < 0; y++){ 
-                Board[x + (boardSize/2) - 1, y + (boardSize/2)] = new CropCell(x, y);
+        for(int x = SECOND_HALF_START ; x < SECOND_HALF_END; x++){ //Right half
+            for(int y = FIRST_HALF_START; y < FIRST_HALF_END; y++){ 
+                Board[x + (UNIT_TILE) - 1, y + (UNIT_TILE)] = new CropCell(x, y);
              }
-             for(int y = 1 ; y < ((boardSize/2) + 1); y++){ 
-                Board[x + (boardSize/2) - 1, y + (boardSize/2) - 1] = new CropCell(x, y);
+             for(int y = SECOND_HALF_START ; y < SECOND_HALF_END; y++){ 
+                Board[x + (UNIT_TILE) - 1, y + (UNIT_TILE) - 1] = new CropCell(x, y);
              }
         }
         //RegenerateBoard();
@@ -39,39 +46,39 @@ public class CropManager : MonoBehaviour
         
     }
     void RegenerateBoard(){
-        for(int i = 0; i < boardSize; i++){
-            for(int j = 0; j < boardSize; j++){
+        for(int i = 0; i < BOARD_SIZE; i++){
+            for(int j = 0; j < BOARD_SIZE; j++){
                 Board[i, j].Plant(); //To do: Only generate crops with a 30% chance
             }
         }
     }
-    public void PlayerPlant(float playerx, float playery){
-        CropCell theCell;
-        for(int x = 0; x < boardSize; x++){
-            for(int y = 0; y < boardSize; y++){
-                theCell = Board[x, y];
-                (float realx, float realy) = theCell.getRealCoordinates();
-                if((Math.Abs((realx - playerx)) < 1) && (Math.Abs((realy - playery)) < 1)){
+    public void PlayerPlant(float playerX, float playerY){
+        CropCell cell;
+        for(int x = 0; x < BOARD_SIZE; x++){
+            for(int y = 0; y < BOARD_SIZE; y++){
+                cell = Board[x, y];
+                (float realX, float realY) = cell.getRealCoordinates();
+                if((Math.Abs((realX - playerX)) < 1) && (Math.Abs((realY - playerY)) < 1)){
                     bool result = Board[x, y].Plant();
-                    if(result == true){
-                        x = boardSize;
-                        y = boardSize;
+                    if(result) {
+                        x = BOARD_SIZE;
+                        y = BOARD_SIZE;
                     }
                 }
             }
         }   
     }
-    public void PlayerHarvest(float playerx, float playery){
-        CropCell theCell;
-        for(int x = 0; x < boardSize; x++){
-            for(int y = 0; y < boardSize; y++){
-                theCell = Board[x, y];
-                (float realx, float realy) = theCell.getRealCoordinates();
-                if((Math.Abs((realx - playerx)) < 1) && (Math.Abs((realy - playery)) < 1)){
+    public void PlayerHarvest(float playerX, float playerY){
+        CropCell cell;
+        for(int x = 0; x < BOARD_SIZE; x++){
+            for(int y = 0; y < BOARD_SIZE; y++){
+                cell = Board[x, y];
+                (float realX, float realY) = cell.getRealCoordinates();
+                if((Math.Abs((realX - playerX)) < 1) && (Math.Abs((realY - playerY)) < 1)){
                     bool result = Board[x, y].Harvest();
-                    if(result == true){
-                        x = boardSize;
-                        y = boardSize;
+                    if(result){
+                        x = BOARD_SIZE;
+                        y = BOARD_SIZE;
                     }
                 }
             }
@@ -81,14 +88,14 @@ public class CropManager : MonoBehaviour
         GameObject cropObject;
         int sunLevel;
         int waterLevel;
-        public float xpos;
-        public float ypos;
-        public CropCell(int xpos, int ypos){
+        public float xPos;
+        public float yPos;
+        public CropCell(int xPos, int yPos){
             cropObject = null; // Null means no seed or plant there
             sunLevel = 0;
             waterLevel = 0;
-            this.xpos = xpos;
-            this.ypos = ypos;
+            this.xPos = xPos;
+            this.yPos = yPos;
         }
         public void ResetCell(){
             sunLevel = 0;
@@ -102,35 +109,31 @@ public class CropManager : MonoBehaviour
                 cropObject = new GameObject("crop");
                 return true;
             }
-            else{
-                return false;
-            }
+            return false;
         }
         public bool Harvest(){
             if(cropObject != null){
                 Destroy(cropObject);
                 return true;
             }
-            else{
-                return false;
-            }
+            return false;
         }
-        public (float xpos, float ypos) getRealCoordinates(){
-            float realxpos = this.xpos;
-            float realypos = this.ypos;
-            if(realxpos > 0){
-                realxpos = (float)(realxpos - 0.5);
+        public (float xPos, float yPos) getRealCoordinates(){
+            float realXPos = this.xPos;
+            float realYPos = this.yPos;
+            if(realXPos > 0){
+                realXPos = (float)(realXPos - 0.5);
             }
             else{
-                realxpos = (float)(realxpos + 0.5);
+                realXPos = (float)(realXPos + 0.5);
             }
-            if(realypos > 0){
-                realypos = (float)(realypos - 0.5);
+            if(realYPos > 0){
+                realYPos = (float)(realYPos - 0.5);
             }
             else{
-                realypos = (float)(realypos + 0.5);
+                realYPos = (float)(realYPos + 0.5);
             }
-            return (realxpos, realypos);
+            return (realXPos, realYPos);
         }
 
     }
