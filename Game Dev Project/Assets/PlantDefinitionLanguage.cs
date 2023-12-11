b6534000ce4304916c324ce3008d4d0738d4999f
Joshua Widjaja <josh@widjaja.us>
2023-12-10 16:33:54 -0800

    fixed compiler errors

--- Game Dev Project/Assets/PlantDefinitionLanguage.cs
+++ Game Dev Project/Assets/PlantDefinitionLanguage.cs
@@ -1,12 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlantDefinitionLanguage : MonoBehaviour
 {
-    public void Name(string name);
-    public void Color(string color);
-    public void GrowsWhen(Func<GrowthContext, bool> growsWhen);
+    public void Name(string name)
+    {
+
+    }
+    public void Color(string color)
+    {
+
+    }
+    public void GrowsWhen(Func<GrowthContext, bool> growsWhen)
+    {
+
+    }
 }
 
 public class GrowthContext
