6a6d595dd485f0ec6bed6da290040d24d88e5908
Joshua Widjaja <josh@widjaja.us>
2023-12-13 20:35:13 -0800

    renamed variables in MakeButton

--- Game Dev Project/Assets/UICreator.cs
+++ Game Dev Project/Assets/UICreator.cs
@@ -22,68 +22,44 @@ public class UICreator : MonoBehaviour
         myCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
         canvasObj.AddComponent<CanvasScaler>();
         canvasObj.AddComponent<GraphicRaycaster>();
-        /*GameObject up = new GameObject();
-        up.name = "UP";
-        up.AddComponent<UnityEngine.UI.Button>();
-        up.AddComponent<RectTransform>();
-        UnityEngine.UI.Button upButton = up.GetComponent<UnityEngine.UI.Button>();
-        up.AddComponent<UnityEngine.UI.Image>();
-        RectTransform upTransform = up.GetComponent<RectTransform>();
-        UnityEngine.UI.Button.ButtonClickedEvent myEvent = new UnityEngine.UI.Button.ButtonClickedEvent();
-        myEvent.AddListener(GameObject.Find("Player").GetComponent<PlayerController>().MoveUp);
-        upButton.onClick = myEvent;
-        //Debug.Log(GameObject.Find("Canvas").transform.Find("Up").transform.Find("Text (TMP)").GetComponents());
-        up.transform.parent = myCanvas.transform;
-        upTransform.anchoredPosition = new Vector3(-283, -105);
-        upTransform.sizeDelta = new Vector2(30, 30);
-        GameObject theText = new GameObject();
-        theText.transform.parent = up.transform;
-        theText.AddComponent<TextMeshProUGUI>();
-        TextMeshProUGUI textComponent = theText.GetComponent<TextMeshProUGUI>();
-        RectTransform textTransform = theText.GetComponent<RectTransform>();
-        textTransform.anchoredPosition = new Vector2(0, 0);
-        textTransform.sizeDelta = new Vector2(30, 30);
-        textComponent.fontSize = 24;
-        textComponent.SetText("↑");
-        textComponent.alignment = TextAlignmentOptions.Center;
-        textComponent.color = Color.black;*/
-        makeButton("UP", -283, -105, 30, 30, "↑", GameObject.Find("Player").GetComponent<PlayerController>().MoveUp);
-        makeButton("LEFT", -315, -139, 30, 30, "←", GameObject.Find("Player").GetComponent<PlayerController>().MoveLeft);
-        makeButton("DOWN", -283, -170, 30, 30, "↓", GameObject.Find("Player").GetComponent<PlayerController>().MoveDown);
-        makeButton("RIGHT", -251, -139, 30, 30, "→", GameObject.Find("Player").GetComponent<PlayerController>().MoveRight);
-        makeButton("Plant", -283, -05, 30, 30, "P", GameObject.Find("Player").GetComponent<PlayerController>().Plant);
-        makeButton("Harvest", -283, 95, 30, 30, "H", GameObject.Find("Player").GetComponent<PlayerController>().Harvest);
-        makeButton("Undo", 483, -05, 30, 30, "U", GameObject.Find("Player").GetComponent<PlayerController>().Undo);
-        makeButton("Redo", 483, 95, 30, 30, "R", GameObject.Find("Player").GetComponent<PlayerController>().Redo);
+        
+        MakeButton("UP", -283, -105, 30, 30, "↑", GameObject.Find("Player").GetComponent<PlayerController>().MoveUp);
+        MakeButton("LEFT", -315, -139, 30, 30, "←", GameObject.Find("Player").GetComponent<PlayerController>().MoveLeft);
+        MakeButton("DOWN", -283, -170, 30, 30, "↓", GameObject.Find("Player").GetComponent<PlayerController>().MoveDown);
+        MakeButton("RIGHT", -251, -139, 30, 30, "→", GameObject.Find("Player").GetComponent<PlayerController>().MoveRight);
+        MakeButton("Plant", -283, -05, 30, 30, "P", GameObject.Find("Player").GetComponent<PlayerController>().Plant);
+        MakeButton("Harvest", -283, 95, 30, 30, "H", GameObject.Find("Player").GetComponent<PlayerController>().Harvest);
+        MakeButton("Undo", 483, -05, 30, 30, "U", GameObject.Find("Player").GetComponent<PlayerController>().Undo);
+        MakeButton("Redo", 483, 95, 30, 30, "R", GameObject.Find("Player").GetComponent<PlayerController>().Redo);
     }
-    GameObject makeButton(string name, int pos1, int pos2, int size1, int size2, string text, UnityEngine.Events.UnityAction theFunction){
-        GameObject up = new GameObject();
-        up.name = name;
-        up.AddComponent<UnityEngine.UI.Button>();
-        up.AddComponent<RectTransform>();
-        UnityEngine.UI.Button upButton = up.GetComponent<UnityEngine.UI.Button>();
-        up.AddComponent<UnityEngine.UI.Image>();
-        RectTransform upTransform = up.GetComponent<RectTransform>();
-        UnityEngine.UI.Button.ButtonClickedEvent myEvent = new UnityEngine.UI.Button.ButtonClickedEvent();
-        //myEvent.AddListener(GameObject.Find("Player").GetComponent<PlayerController>().MoveUp);
-        myEvent.AddListener(theFunction);
-        upButton.onClick = myEvent;
-        //Debug.Log(GameObject.Find("Canvas").transform.Find("Up").transform.Find("Text (TMP)").GetComponents());
-        up.transform.parent = myCanvas.transform;
-        upTransform.anchoredPosition = new Vector3(pos1, pos2);
-        upTransform.sizeDelta = new Vector2(size1, size2);
-        GameObject theText = new GameObject();
-        theText.transform.parent = up.transform;
-        theText.AddComponent<TextMeshProUGUI>();
-        TextMeshProUGUI textComponent = theText.GetComponent<TextMeshProUGUI>();
-        RectTransform textTransform = theText.GetComponent<RectTransform>();
+    GameObject MakeButton(string name, int pos1, int pos2, int size1, int size2, string text, UnityEngine.Events.UnityAction theFunction){
+        GameObject uiObject = new GameObject();
+        uiObject.name = name;
+        uiObject.AddComponent<UnityEngine.UI.Button>();
+        uiObject.AddComponent<RectTransform>();
+        UnityEngine.UI.Button uiButton = uiObject.GetComponent<UnityEngine.UI.Button>();
+        uiObject.AddComponent<UnityEngine.UI.Image>();
+        RectTransform uiTransform = uiObject.GetComponent<RectTransform>();
+        UnityEngine.UI.Button.ButtonClickedEvent clickedEvent = new UnityEngine.UI.Button.ButtonClickedEvent();
+        clickedEvent.AddListener(theFunction);
+        uiButton.onClick = clickedEvent;
+        uiObject.transform.parent = myCanvas.transform;
+
+        uiTransform.anchoredPosition = new Vector3(pos1, pos2);
+        uiTransform.sizeDelta = new Vector2(size1, size2);
+        GameObject uiText = new GameObject();
+        uiText.transform.parent = uiObject.transform;
+        uiText.AddComponent<TextMeshProUGUI>();
+        TextMeshProUGUI textComponent = uiText.GetComponent<TextMeshProUGUI>();
+
+        RectTransform textTransform = uiText.GetComponent<RectTransform>();
         textTransform.anchoredPosition = new Vector2(0, 0);
         textTransform.sizeDelta = new Vector2(30, 30);
         textComponent.fontSize = 24;
         textComponent.SetText(text);
         textComponent.alignment = TextAlignmentOptions.Center;
         textComponent.color = Color.black;
-        return up;
+        return uiObject;
     }
     // Update is called once per frame
     void Update()
